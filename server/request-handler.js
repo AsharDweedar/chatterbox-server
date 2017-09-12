96a3c9c0af94f7c6dbc80c56b3339a6e9966e3ee
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-09-12 13:20:25 +0300

    13 test works :P

--- server/request-handler.js
+++ server/request-handler.js
@@ -13,22 +13,7 @@ this file and include it in basic-server.js so that it actually works.
 **************************************************************/
 var allMessages = [];
 var requestHandler = function(request, response) {
-  var headers = defaultCorsHeaders;
-   headers['Content-Type'] = 'text/plain';
-
-    //JSON.stringify(allMessages);
-    if (request.method === 'GET') {
-      headers['Content-Type'] = 'text/HTML';
-      console.log(request);
-    } else if (request.method === 'POST') {
-          console.log(request)
-          allMessages.push(request.data)
-          console.log(allMessages)
-    } 
-//console.log(response);
-//console.log(request);
-//console.log(request.read());
-  
+   
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -47,15 +32,43 @@ var requestHandler = function(request, response) {
 
   // The outgoing status.
   var statusCode = 200;
+  
 
   // See the note below about CORS headers.
-  /*var headers = defaultCorsHeaders;*/
+  var headers = defaultCorsHeaders;
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  /*headers['Content-Type'] = 'text/plain';*/
+  headers['Content-Type'] = 'text/plain';
+
+
+
+  var toSend;
+    if (request.method === 'GET') {
+      headers['Content-Type'] = 'application/json';
+      toSend = {
+                  results: allMessages
+                }
+      console.log('GET : ');
+      console.log(request);
+      
+    } else if (request.method === 'POST') {
+      allMessages.push(request._postData);
+      toSend = {"results":[]} ;
+      statusCode = 201;
+      console.log('POST : ')
+      console.log( response)
+          
+    } 
+    if (request.url !== '/classes/messages'){
+      statusCode = 404;
+    }
+
+  console.log('all messages : ') 
+  console.log(allMessages) 
+
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
@@ -69,7 +82,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('{"results": ['+allMessages+']}');
+  response.end(JSON.stringify(toSend));
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -87,5 +100,5 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
-exports.requestHandler = requestHandler;
+module.exports.requestHandler = requestHandler;
 
